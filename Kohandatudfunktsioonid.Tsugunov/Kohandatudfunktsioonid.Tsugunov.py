19997be6f7de0524867c4f0da83321cd078a200d
opilane <opilane@HP-CZC2349HTD>
2023-02-10 14:00:12 +0200

    Add project files.

---
+++ Kohandatudfunktsioonid.Tsugunov/Kohandatudfunktsioonid.Tsugunov.py
@@ -0,0 +1,20 @@
+﻿from math import * 
+from random import *
+from module1 import *
+#10/02/23
+vastus=(is_year_leap(input("Aasta: ")))
+print(vastus)
+#või
+print(is_year_leap(input("Aasta: ")))
+
+while True:
+	vastus=arithmetic(float(input("arv1: ")),input("tehe:"),float(input("arv2:")))
+	print(vastus)
+	
+
+
+
+
+
+
+
---
+++ Kohandatudfunktsioonid.Tsugunov/module1.py
@@ -0,0 +1,39 @@
+﻿def arithmetic(arv1:float,tehe:str,arv2:float)->any:
+    """any = означает 
+    """
+    if tehe== "+":
+       vastus=arv1+arv2
+    elif tehe== "-":
+       vastus=arv1-arv2
+    elif tehe== "*":
+       vastus=arv1*arv2
+    elif tehe== "/":
+       if arv2==0:
+           vastus="DIV0"
+       else:
+           vastus=arv1/arv2
+    else:
+        vastus="Tundmatu tehe"
+
+    return vastus
+
+def is_year_leap(aasta: int)->bool:
+    """
+    """
+    if aasta%4==0:
+        t=True
+    else:
+        t=False
+
+    return t
+        
+
+def square(a):
+    #
+	p=4*a
+	s=a*a
+	d=(a**2)/2
+	d= d**2
+	b=(p, s, d)
+print(square(8))
+
