acc90d33a1d24366a5971ed5b115100c63411dd6
TsugunovS <stanislav.tsugunov@gmail.com>
2023-02-13 12:38:37 +0200

    datetime

--- Kohandatudfunktsioonid.Tsugunov/Kohandatudfunktsioonid.Tsugunov.py
+++ Kohandatudfunktsioonid.Tsugunov/Kohandatudfunktsioonid.Tsugunov.py
@@ -1,15 +1,48 @@
 ﻿from math import * 
 from random import *
 from module1 import *
+#13/02/23
+kuupaev=date(int(input("Kuupäev: ")),int(input("Kuu: ")),int(input("Aasata: ")))
+print(kuupaev)
+
+
+
 #10/02/23
-vastus=(is_year_leap(input("Aasta: ")))
-print(vastus)
+
+
+
+
+
+
+#vastus=(is_year_leap(input("Aasta: ")))
+#print(vastus)
 #või
-print(is_year_leap(input("Aasta: ")))
+#print(is_year_leap(input("Aasta: ")))
+
+#3
+a=float(input("Sisestage "))
+print(a)
+
+#6
+a=float(input("Sisestage arv: "))
+print(a)
+
+#5
+b=bank(float(input("Sisestage summa: ")),int(input("Mitu aasta?")))
+print(b)
+
+#4
+month=(float(input("Sisestage kuu: ")))
+print()
+
 
 while True:
 	vastus=arithmetic(float(input("arv1: ")),input("tehe:"),float(input("arv2:")))
 	print(vastus)
+
+
+
+
 	
 
 
--- Kohandatudfunktsioonid.Tsugunov/module1.py
+++ Kohandatudfunktsioonid.Tsugunov/module1.py
@@ -1,4 +1,19 @@
-﻿def arithmetic(arv1:float,tehe:str,arv2:float)->any:
+﻿from datetime import *
+from itertools import filterfalse
+from pickle import FALSE 
+def date(d:int, m:int,y:int)-> bool:
+    try:
+        print(date(y,m,d))
+        flag=True
+    except:
+        print("Viga")
+        flag=False
+    return flag
+
+
+
+
+def arithmetic(arv1:float,tehe:str,arv2:float)->any:
     """any = означает 
     """
     if tehe== "+":
@@ -17,23 +32,68 @@
 
     return vastus
 
-def is_year_leap(aasta: int)->bool:
+#2
+# is_year_leap(aasta: int)->bool:
+#    """
+#    """
+#    if aasta%4==0:
+#        t=True
+#    else:
+#        t=False
+#
+#    return t
+
+#3
+def square(a:float)->float:
+    """pindala läbimõõt diagonale leidmine
+    """
+    p=4*a
+    s=a*a
+    d=(a**2)/2
+    return p,s,d
+
+#4
+def season(month:float)->str:
     """
     """
-    if aasta%4==0:
-        t=True
+    if month == 12 or 1 <= month <= 2:
+       print("Зима")
+    elif  3 <= month <= 5:
+       print("Весна")
+    elif 6 <= month <= 8:
+       print("Лето")
+    elif 9 <= month <= 11:
+       print("Осень")
     else:
-        t=False
+       print("Viga!")
+n = int(input("Введите номер месяца 1-12: "))
+season(n)
 
-    return t
-        
+#5
+def bank(b:float, a:int)->float:
+    """
+    """
+    for i in range (a):
+        b=b+b*0.1
+    return b
 
-def square(a):
-    #
-	p=4*a
-	s=a*a
-	d=(a**2)/2
-	d= d**2
-	b=(p, s, d)
-print(square(8))
+#6
+def is_prime(a:float)->str:
+    if a % a == 0 and a != 0:
+        return True
+    else:
+        return False
+
+def is_prime(arv:float)->str:
+    """
+    """
+    flag=True
+    for i in range(2,arv):
+        if arv%i==0:
+            flag=False
+            break
+    return flag
+
+from datetime import * 
+
 
